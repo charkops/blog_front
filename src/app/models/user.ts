3d4ab7475a08751dc67a6b3f1e76a24ef7cdfbd6
Charis Kopsacheilis <charis.kopsacheilis@gmail.com>
2021-01-19 13:58:16 +0200

    Add authentication service

---
+++ src/app/models/user.ts
@@ -0,0 +1,7 @@
+export class User {
+  user_id: number;
+  username: string;
+  password: string;
+  blog_id: number;
+  token?: string;
+}
---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { User } from '../models/user';
+
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  // A helper - getter for User
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  // Store user info to be logged in even through refreshing the page
+  login(username: string, password: string) {
+    return this.http.post<any>(environment.backendUrl + '/users/authenticate', { username, password })
+      .pipe(map(user => {
+        // Store user details and JWT token in local storage
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        this.currentUserSubject.next(user);
+        return user;
+      }));
+  }
+
+  // Self-explenatory
+  logout() {
+    // Remove user from local storage and set current user to null
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+}
