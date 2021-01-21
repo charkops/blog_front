b929dd78db22bf23dd2d55f7c87f4fa1e0ffef85
Charis Kopsacheilis <charis.kopsacheilis@gmail.com>
2021-01-21 11:19:12 +0200

    Modify /home a bit and now can only be activated if user is logged in

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { AuthenticationService } from '../services/authentication.service';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  currentUser: User;
+
+  constructor(
+    private authenticationService: AuthenticationService
+  ) { }
 
   ngOnInit(): void {
+    this.currentUser = this.authenticationService.currentUserValue;
   }
 
 }
